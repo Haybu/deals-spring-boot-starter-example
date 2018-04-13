937c6ae305a54c14f8bf5e1afecdcfd2588e4e12
Stephane Nicoll <snicoll@pivotal.io>
2018-04-13 10:40:30 +0200

    Field visibility and name
    This commit polishes stuff around field naming and visibility.
    
    1. Fields should be marked as private, and private final where possible
    2. Constructor parameter must have the same name as the field on the
    class
    3. Please don't put single line method body inline
    4. Controversial subject but we do use tabs (this doesn't have to be
    enforced of course but this is about consistency with the rest of the
    portfolio)
    5. ConfigurationProperties object should be standard POJO (i.e.
    getter/setter, no arg constructor)
    6. Do not use field @Autowiring in production code

--- deals/src/main/java/io/agilehandy/deals/Discount.java
+++ deals/src/main/java/io/agilehandy/deals/Discount.java
@@ -11,47 +11,46 @@ import java.util.Random;
 /**
  * By: Haytham Mohamed
  */
-
 public class Discount {
 
-    Category category;
+	private final Category category;
 
-    public Discount(Category c) {
-        category = c;
-    }
+	public Discount(Category category) {
+		this.category = category;
+	}
 
-    public String printDiscountTicket() {
-        String name = category.getName();
+	public String printDiscountTicket() {
+		String name = category.getName();
 
-        List<DayOfWeek> dayOfWeeks = Arrays.asList(DayOfWeek.values());
+		List<DayOfWeek> dayOfWeeks = Arrays.asList(DayOfWeek.values());
 
-        LocalDate date = LocalDate.now();
-        DayOfWeek dow = date.getDayOfWeek();
+		LocalDate date = LocalDate.now();
+		DayOfWeek dow = date.getDayOfWeek();
 
-        int dayIndex = dayOfWeeks.indexOf(dow);
+		int dayIndex = dayOfWeeks.indexOf(dow);
 
-        int categoryNameLength = name.length();
-        int numberOfDigits = String.valueOf(categoryNameLength).length();
+		int categoryNameLength = name.length();
+		int numberOfDigits = String.valueOf(categoryNameLength).length();
 
-        float fraction = categoryNameLength % (10 * numberOfDigits);
+		float fraction = categoryNameLength % (10 * numberOfDigits);
 
-        float discount = randFloat(0, dayIndex * fraction);
+		float discount = randFloat(0, dayIndex * fraction);
 
-        DecimalFormat f = new DecimalFormat("#0.00");
+		DecimalFormat f = new DecimalFormat("#0.00");
 
-        return new StringBuffer("Today ").append(dow)
-                .append(", your ticket for ")
-                .append(name)
-                .append(" offers ")
-                .append(f.format(discount))
-                .append("% discount")
-                .toString();
-    }
+		return new StringBuffer("Today ").append(dow)
+				.append(", your ticket for ")
+				.append(name)
+				.append(" offers ")
+				.append(f.format(discount))
+				.append("% discount")
+				.toString();
+	}
 
-    private static float randFloat(float min, float max) {
-        Random rand = new Random();
-        float result = rand.nextFloat() * (max - min) + min;
-        return result;
-    }
+	private static float randFloat(float min, float max) {
+		Random rand = new Random();
+		float result = rand.nextFloat() * (max - min) + min;
+		return result;
+	}
 
 }
